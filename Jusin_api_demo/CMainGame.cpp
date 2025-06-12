51269ec2d50b610008e42ed743f455b8697d0690
Jaehyeok-Soh <revol3154@gmail.com>
2025-06-12 20:33:54 +0900

    Initial commit

---
+++ Jusin_api_demo/CMainGame.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CMainGame.h"
+
+CMainGame::CMainGame()
+{
+}
+
+CMainGame::~CMainGame()
+{
+}
+
+void CMainGame::Initialize()
+{
+	m_DC = GetDC(g_hWnd);
+}
+
+void CMainGame::Update()
+{
+}
+
+void CMainGame::Late_Update()
+{
+}
+
+void CMainGame::Render()
+{
+}
+
+void CMainGame::Release()
+{
+}
---
+++ Jusin_api_demo/Define.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define WINCX 1000
+#define WINCY 800
+
+extern HWND g_hWnd;
