b12d13427b3b0fc60abe9b715d322a580ed9de41
Jaehyeok-Soh <revol3154@gmail.com>
2025-06-25 21:53:47 +0900

    what

---
+++ Jusin_api_demo_editor/CLoadingSpinner.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "CLoadingSpinner.h"
+
+CLoadingSpinner::CLoadingSpinner()
+{
+}
+
+CLoadingSpinner::~CLoadingSpinner()
+{
+}
+
+void CLoadingSpinner::Initialize()
+{
+    //loadingscreen_spinner
+}
+
+int CLoadingSpinner::Update()
+{
+    return NOEVENT;
+}
+
+void CLoadingSpinner::Late_Update()
+{
+}
+
+void CLoadingSpinner::Render(HDC _dc)
+{
+}
+
+void CLoadingSpinner::Release()
+{
+}
---
+++ Jusin_api_demo_editor/CLoadingSpinner.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CObject.h"
+
+class CLoadingSpinner
+	: public CObject
+{
+public:
+	CLoadingSpinner();
+	~CLoadingSpinner();
+
+public:
+	void Initialize() override;
+	int Update() override;
+	void Late_Update() override;
+	void Render(HDC _dc) override;
+	void Release() override;
+};
